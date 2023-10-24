fc63b4413c3b70bcf25590ab4b84450cf57143f7
Michael-7274 <michaeldanieal749@gmail.com>
2023-10-24 15:00:06 +0530

    TicTacToe-turn state updated and warnings removed

--- src/myComponents/ticTacToe/ChildButton.js
+++ src/myComponents/ticTacToe/ChildButton.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react'
 import './style.css'
 export default function ChildButton({ boxValueAndIndexArray, updateTurn }) {//needs optimization, need better naming 
 
--- src/myComponents/ticTacToe/ParentBoard.js
+++ src/myComponents/ticTacToe/ParentBoard.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import ChildButton from './ChildButton';
 import './style.css'
-export default function ParentBoard() {//reduce state and props
-    const [turnCount, increaseTurnCount] = useState(0);
+export default function ParentBoard() {
+    const[turn,setTurn]=useState('X');//say deliberately as X or O
     const [boxValues, setBoxValues] = useState(['', '', '', '', '', '', '', '', '']);
     const [result, setResult] = useState('');
     const winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
@@ -14,12 +14,14 @@ export default function ParentBoard() {//reduce state and props
 
     //this function updates the turn each time when a button is clicked
     function changingTurn(index) {
-        if (boxValues[index] == '' && result == '') {
-            if (turnCount % 2 == 0) {
+        if (boxValues[index] === '' && result === '') {
+            if (turn==='X') {
                 setBoxArray(index, 'X')
+                setTurn('O');
             }
             else {
                 setBoxArray(index, "O")
+                setTurn('X');
             }
         }
     }
@@ -29,17 +31,15 @@ export default function ParentBoard() {//reduce state and props
         let tempArr = [...boxValues]
         tempArr[index] = value;
         setBoxValues(tempArr);
-        increaseTurnCount(turnCount + 1);
-        console.log(turnCount);
     }
 
     //this function checks if the winning condition is satisfied based on the turn in which a button is clicked
     function findWinner() {
         for (const condition of winConditions) {
             const [a, b, c] = condition;
-            if ((boxValues[a] != '') && (boxValues[a] === boxValues[b]) && (boxValues[b] === boxValues[c])) {
+            if ((boxValues[a] !== '') && (boxValues[a] === boxValues[b]) && (boxValues[b] === boxValues[c])) {
 
-                if ((turnCount % 2) == 1) {
+                if (turn === 'O') {
                     setResult('X wins')
                     setTimeout(function () {
                         window.location.reload(true);
@@ -55,7 +55,7 @@ export default function ParentBoard() {//reduce state and props
                 }
                 break;
             }
-            else if (turnCount == 9) {
+            else if (boxValues.indexOf('')===-1) {
                 setResult("Draw")
                 setTimeout(function () {
                     window.location.reload(true);
@@ -77,7 +77,7 @@ export default function ParentBoard() {//reduce state and props
                 <h1>TIC-TAC-TOE</h1>
             </div>
             <div className='centerdiv'>
-                {((turnCount == 9) || (result != '')) ? <h1>Match over</h1> : ((turnCount % 2 == 0) ?
+                {(boxValues.indexOf('')===-1 || (result !== '')) ? <h1>Match over</h1> : ((turn === 'X') ?
                     <h1>X Turn</h1> : <h1>O Turn</h1>)}
             </div>
             <div className='centerdiv'>
